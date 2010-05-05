57a68986cb02262f22b4ec32e3d97b9b8b6966f6
stanleyhuang <stanleyhuang@6dd02fe3-967f-4638-bac9-8d06f6e08789>
2010-05-05 10:01:20 +0000

    initial import
    git-svn-id: https://obdcon.svn.sourceforge.net/svnroot/obdcon@1 6dd02fe3-967f-4638-bac9-8d06f6e08789

---
+++ main.cpp
@@ -0,0 +1,63 @@
+#include "ctb.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <iostream>
+#include <string>
+
+using namespace ctb;
+using namespace std;
+
+static const char* options="b:d:e:hp:t:";
+static const char* helpMessage =
+{
+    "A simple serial port class test\n"
+    "ctbtest [options]\n"
+    "available options are:\n"
+    "-a     : address (only GPIB)\n"
+    "-b     : baudrate [any value], default is 38400\n"
+    "-d     : connected device, default is COM1\n"
+    "-e     : eos\n"
+    "-h     : print this\n"
+    "-p     : protocol like 8N1\n"
+    "-t     : communication timeout in ms (default is 100ms)\n"
+};
+
+int main(int argc, char* argv[])
+{
+	ctb::IOBase* device = NULL;
+    int baudrate = 115200;
+    string devname = ctb::COM1;
+    string eos = "\r";
+    string protocol = "8N1";
+    int timeout = 200;
+    int quit = 0;
+    int val;
+
+    while ( ( val=getopt( argc, argv, (char*)options ) ) != EOF ) {
+	   switch ( val ) {
+	   case 'b' : baudrate = strtol( optarg, NULL, 10 ); break;
+	   case 'd' : devname = optarg; break;
+	   case 'h' : cerr << helpMessage << endl; exit( 0 );
+	   case 'p' : protocol = optarg; break;
+	   case 't' : timeout = strtol( optarg, NULL, 10 ); break;
+	   }
+    }
+
+	ctb::SerialPort* serialPort = new ctb::SerialPort();
+
+ 	if( serialPort->Open( devname.c_str(), baudrate, 
+					protocol.c_str(), 
+					ctb::SerialPort::NoFlowControl ) >= 0 ) {
+
+		device = serialPort;
+
+	} else {
+		fprintf(stderr, "Error opening %s port\n", devname);
+		return -1;
+	}
+
+	return 0;
+}
