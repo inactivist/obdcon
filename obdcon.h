70d4bf26dc9b8353c15010ade37aa6dfa4ae6950
stanleyhuang <stanleyhuang@6dd02fe3-967f-4638-bac9-8d06f6e08789>
2012-02-19 15:58:45 +0000

    import Arduino OBD library and sample
    git-svn-id: https://obdcon.svn.sourceforge.net/svnroot/obdcon@41 6dd02fe3-967f-4638-bac9-8d06f6e08789

--- obdcon.h
+++
@@ -1,7 +0,0 @@
-/*************************************************************************
-* libobd - The OBD-II access library
-* Distributed under MPL 1.1
-*
-* Copyright (c) 2010 Stanley Huang <stanleyhuangyc@gmail.com>
-* All rights reserved.
-**************************************************************************/
