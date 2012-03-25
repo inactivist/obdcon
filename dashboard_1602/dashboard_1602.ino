13bbc8e23baecbd53659f209b689e3e6e5e7625f
stanleyhuang <stanleyhuang@6dd02fe3-967f-4638-bac9-8d06f6e08789>
2012-03-25 14:15:20 +0000

    git-svn-id: https://obdcon.svn.sourceforge.net/svnroot/obdcon@58 6dd02fe3-967f-4638-bac9-8d06f6e08789

--- dashboard_1602/dashboard_1602.ino
+++ dashboard_1602/dashboard_1602.ino
@@ -21,8 +21,8 @@ COBD obd;
 unsigned int  adc_key_val[5] ={30, 150, 360, 535, 760 };
 int NUM_KEYS = 5;
 int adc_key_in;
-int key=-1;
-int oldkey=-1;
+char key=-1;
+char oldkey=-1;
 unsigned long lastTick = millis();
 uint8_t modes[2] = {0, 2};
 
@@ -164,21 +164,12 @@ void loop()
 }
 
 // Convert ADC value to key number
-int get_key(unsigned int input)
+char get_key(unsigned int input)
 {
-	int k;
-
-	for (k = 0; k < NUM_KEYS; k++)
-	{
-		if (input < adc_key_val[k])
-		{
-
-    return k;
-        }
-	}
-
-    if (k >= NUM_KEYS)
-        k = -1;     // No valid key pressed
-
-    return k;
+  char k;
+  for (k = 0; k < NUM_KEYS; k++) {
+    if (input < adc_key_val[k])
+      return k;
+  }
+  return -1;
 }
