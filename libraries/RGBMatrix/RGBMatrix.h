7b74b1c29409fecabc2273747cf482ddd554ca99
stanleyhuang <stanleyhuang@6dd02fe3-967f-4638-bac9-8d06f6e08789>
2012-11-16 12:15:51 +0000

    add RGB matrix module library
    git-svn-id: https://obdcon.svn.sourceforge.net/svnroot/obdcon@69 6dd02fe3-967f-4638-bac9-8d06f6e08789

---
+++ libraries/RGBMatrix/RGBMatrix.h
@@ -0,0 +1,118 @@
+#ifndef RGBMATRIX_H
+#define RGBMATRIX_H
+
+#define CMD_FRAME 0x51
+#define CMD_PIXEL 0x52
+#define CMD_DIGIT 0x53
+#define CMD_DRAW 0x54
+#define CMD_CLEAR 0x55
+
+class CFrameBuffer
+{
+public:
+    CFrameBuffer() { Clear(); }
+    void SetPixel(uint8_t x, uint8_t y, uint8_t r, uint8_t g, uint8_t b)
+    {
+        if ((x & 1)) {
+            x >>= 1;
+            framebuffer[0][y][x] = (g >> 4) | (framebuffer[0][y][x] & 0xF0);
+            framebuffer[1][y][x] = (r >> 4) | (framebuffer[1][y][x] & 0xF0);
+            framebuffer[2][y][x] = (b >> 4) | (framebuffer[2][y][x] & 0xF0);
+        } else {
+            x >>= 1;
+            framebuffer[0][y][x] = (g & 0xF0) | (framebuffer[0][y][x] & 0x0F);
+            framebuffer[1][y][x] = (r & 0xF0) | (framebuffer[1][y][x] & 0x0F);
+            framebuffer[2][y][x] = (b & 0xF0) | (framebuffer[2][y][x] & 0x0F);
+        }
+    }
+    void ClearPixel(uint8_t x, uint8_t y)
+    {
+        if ((x & 1)) {
+            x >>= 1;
+            framebuffer[0][y][x] &= 0xF0;
+            framebuffer[1][y][x] &= 0xF0;
+            framebuffer[2][y][x] &= 0xF0;
+        } else {
+            x >>= 1;
+            framebuffer[0][y][x] &= 0x0F;
+            framebuffer[1][y][x] &= 0x0F;
+            framebuffer[2][y][x] &= 0x0F;
+        }
+    }
+    void Clear()
+    {
+        memset(framebuffer, 0, sizeof(framebuffer));
+    }
+    uint8_t framebuffer[3][8][4];
+};
+
+class CRGBMatrixI2C
+{
+public:
+    void Init(uint8_t addr)
+    {
+        m_addr = addr;
+        Wire.begin();
+    }
+    void SetFrame(CFrameBuffer& frame)
+    {
+        uint8_t* pixels = (uint8_t*)frame.framebuffer;
+        // send command
+        Wire.beginTransmission(m_addr);
+        Wire.write((uint8_t)CMD_FRAME);
+        Wire.endTransmission();
+        // send R plane
+        Wire.beginTransmission(m_addr);
+        Wire.write(pixels, 32);
+        Wire.endTransmission();
+        // send G plane
+        Wire.beginTransmission(m_addr);
+        Wire.write(pixels + 32, 32);
+        Wire.endTransmission();
+        // send B plane
+        Wire.beginTransmission(m_addr);
+        Wire.write(pixels + 64, 32);
+        Wire.endTransmission();
+    }
+    void SetPixel(uint8_t x, uint8_t y, uint8_t r, uint8_t g, uint8_t b)
+    {
+        // send command
+        Wire.beginTransmission(m_addr);
+        Wire.write((uint8_t)CMD_PIXEL);
+        Wire.write((x & 0xf) | (y << 4));
+        Wire.write(r);
+        Wire.write(g);
+        Wire.write(b);
+        Wire.endTransmission();
+    }
+    void SetChar(char c, char bias_x, char bias_y, uint8_t r, uint8_t g, uint8_t b)
+    {
+        // send command uint8_t
+        Wire.beginTransmission(m_addr);
+        Wire.write((uint8_t)CMD_DIGIT);
+        Wire.write(c);
+        Wire.write(bias_x);
+        Wire.write(bias_y);
+        Wire.write(r);
+        Wire.write(g);
+        Wire.write(b);
+        Wire.endTransmission();
+    }
+    void Draw()
+    {
+        Wire.beginTransmission(m_addr);
+        Wire.write((uint8_t)CMD_DRAW);
+        Wire.endTransmission();
+    }
+
+    void Clear()
+    {
+        Wire.beginTransmission(m_addr);
+        Wire.write((uint8_t)CMD_CLEAR);
+        Wire.endTransmission();
+    }
+private:
+    uint8_t m_addr;
+};
+
+#endif
