0daa47b6b5d0ed122a46c08709e17116b01f0217
stanleyhuang <stanleyhuang@6dd02fe3-967f-4638-bac9-8d06f6e08789>
2012-11-19 08:13:01 +0000

    add numeric digit display APIs
    git-svn-id: https://obdcon.svn.sourceforge.net/svnroot/obdcon@70 6dd02fe3-967f-4638-bac9-8d06f6e08789

--- libraries/RGBMatrix/RGBMatrix.h
+++ libraries/RGBMatrix/RGBMatrix.h
@@ -3,9 +3,11 @@
 
 #define CMD_FRAME 0x51
 #define CMD_PIXEL 0x52
-#define CMD_DIGIT 0x53
-#define CMD_DRAW 0x54
-#define CMD_CLEAR 0x55
+#define CMD_CHAR_5X7 0x53
+#define CMD_DIGIT_3X5 0x54
+#define CMD_DIGIT_3X7 0x55
+#define CMD_DRAW 0x60
+#define CMD_CLEAR 0x61
 
 class CFrameBuffer
 {
@@ -76,7 +78,6 @@ public:
     }
     void SetPixel(uint8_t x, uint8_t y, uint8_t r, uint8_t g, uint8_t b)
     {
-        // send command
         Wire.beginTransmission(m_addr);
         Wire.write((uint8_t)CMD_PIXEL);
         Wire.write((x & 0xf) | (y << 4));
@@ -85,12 +86,35 @@ public:
         Wire.write(b);
         Wire.endTransmission();
     }
-    void SetChar(char c, char bias_x, char bias_y, uint8_t r, uint8_t g, uint8_t b)
+    void ShowChar5x7(char c, char bias_x, char bias_y, uint8_t r, uint8_t g, uint8_t b)
     {
-        // send command uint8_t
         Wire.beginTransmission(m_addr);
-        Wire.write((uint8_t)CMD_DIGIT);
+        Wire.write((uint8_t)CMD_CHAR_5X7);
         Wire.write(c);
+        Wire.write(bias_x);
+        Wire.write(bias_y);
+        Wire.write(r);
+        Wire.write(g);
+        Wire.write(b);
+        Wire.endTransmission();
+    }
+    void ShowDigit3x5(byte n, char bias_x, char bias_y, uint8_t r, uint8_t g, uint8_t b)
+    {
+        Wire.beginTransmission(m_addr);
+        Wire.write((uint8_t)CMD_DIGIT_3X5);
+        Wire.write(n);
+        Wire.write(bias_x);
+        Wire.write(bias_y);
+        Wire.write(r);
+        Wire.write(g);
+        Wire.write(b);
+        Wire.endTransmission();
+    }
+    void ShowDigit3x7(byte n, char bias_x, char bias_y, uint8_t r, uint8_t g, uint8_t b)
+    {
+        Wire.beginTransmission(m_addr);
+        Wire.write((uint8_t)CMD_DIGIT_3X7);
+        Wire.write(n);
         Wire.write(bias_x);
         Wire.write(bias_y);
         Wire.write(r);
